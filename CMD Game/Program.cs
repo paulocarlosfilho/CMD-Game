243dcad3f98d6521a6c49aea5f7f9122591f9a75
paulocarlosfilho <83230882+paulocarlosfilho@users.noreply.github.com>
2021-10-01 14:51:13 -0300

    Adicionar arquivos de projeto.

---
+++ CMD Game/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CMD_Game
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Grid grid = new Grid();
+            
+            grid.Preencher();
+        }
+    }
+}
