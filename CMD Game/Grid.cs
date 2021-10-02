40902938a591361190d1db2798723da8a16d80d3
Beatriz Silva <71887281+BeatrizFS@users.noreply.github.com>
2021-10-02 01:09:05 -0300

    add heroi, destino e option

--- CMD Game/Grid.cs
+++ CMD Game/Grid.cs
@@ -8,18 +8,104 @@ namespace CMD_Game
 {
     public class Grid
     {
-        int[,] grid = new int[20, 20];
+        static string[,] grid = new string[20, 20]
+        {
+            {"O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ",},
+            {"O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ",},
+            {"O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ",},
+            {"O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ",},
+            {"O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ",},
+            {"O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ",},
+            {"O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ",},
+            {"O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ",},
+            {"O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ",},
+            {"O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ",},
+            {"O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ",},
+            {"O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ",},
+            {"O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ",},
+            {"O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ",},
+            {"O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ",},
+            {"O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ",},
+            {"O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ",},
+            {"O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ",},
+            {"O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ",},
+            {"O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ","O ",},
+
+        };
+
+        static string heroi = "H ";
+        static int heroiLinha = 0;
+        static int heroiColuna = 0;
+        static string destino = "D";
+        static int destinoLinha = 19;
+        static int destinoColuna = 19;
 
         public void Preencher()
         {
+
+            grid[0, 0] = heroi;
+            grid[19, 19] = destino;
+
             for (int i = 0; i < 20; i++)
             {
                 for (int j = 0; j < 20; j++)
                 {
-                   Console.Write($"{grid[i,j]} ");
+                    Console.Write(grid[i, j]);
                 }
+
                 Console.WriteLine();
             }
+            Console.WriteLine();
+            Console.WriteLine("========================================");
+        }
+
+        Option option = new Option();
+        public void Escolhar()
+            {
+                string comando = Console.ReadLine().ToUpper();
+                //ToUpper = converte as letras para maiusculo
+
+                //Possiveis resultados dos comandos
+                if (comando == "[A] to move left.    ")
+                {
+                    if (grid[heroiLinha, heroiColuna - 1] != " ")
+                    {
+                        grid[heroiLinha, heroiColuna] = "O ";
+                        heroiLinha--;
+                    }
+                }
+                else if (comando == "[D] to move right.")
+                {
+                    if (grid[heroiLinha, heroiColuna + 1] != " ")
+                    {
+                        grid[heroiLinha, heroiColuna] = "O ";
+                        heroiLinha++;
+                    }
+                }
+                else if (comando == "[W] to move up.      ")
+                {
+                    if (grid[heroiLinha - 1, heroiColuna] != " ")
+                    {
+                        grid[heroiLinha, heroiColuna] = "O ";
+                        heroiLinha--;
+                    }
+
+                }
+                else if (comando == "[S] to move down.")
+                {
+                    if (grid[heroiLinha + 1, heroiColuna] != " ")
+                    {
+                        grid[heroiLinha, heroiColuna] = "O ";
+                        heroiLinha++;
+                        Console.Write("Tecla S precionada");
+                    }
+                }
+                else if (comando == "[SPACE] to attack")
+                {
+
+                }
+
+                grid[heroiLinha, heroiColuna] = heroi;
         }
 
     }
--- CMD Game/Program.cs
+++ CMD Game/Program.cs
@@ -5,13 +5,18 @@ namespace CMD_Game
     
     class Program
     {
+        static string nomeDoJogo = "CDM Game";
+
         static void Main(string[] args)
         {
+            Console.Title = nomeDoJogo;
 
             //Iniciar grid
-            Grid grid = new Grid();
-            
+            Grid grid = new Grid();           
             grid.Preencher();
+
+            Option option = new Option();
+            option.Escolhar();
 
             //-----------------------------------------------
             
